46c15d0f46139bce54b7d67501a16f5806d0b1e3
Caleb Harrington <WMUcaleb>
2021-11-29 15:55:56 -0500

    Added Map

---
+++ Blendr/Blendr/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+// MapViewController.swift
+// Blendr //
+// Created by Caleb R Harrington on 11/29/21. //
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 42.283178, longitude: -85.615219) // Make the latitude that of the restaraunt
+        annotation.title = "Western Michigan" // Make this the name of the restaraunt
+        mapView.addAnnotation(annotation)
+        let region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+
+        
+        // Do any additional setup after loading the view. }
+/*
+// MARK: - Navigation
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) { // Get the new view controller using segue.destination.
+// Pass the selected object to the new view controller.
+} */
+    }
+}
